28bad0a2b36a834b61c15b8e175e70101f58f8f0
Andrew Hutton <fromalaska49@gmail.com>
2016-09-17 14:03:19 -0500

    Created index page

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>
+			Hello World
+		</title>
+	</head>
+	<body>
+		<h1>
+			The server is working!
+		</h1>
+		<br />
+		<?php
+			echo('<!--');
+		?>
+		<p>
+			Error: PHP isn't working!
+		</p>
+		<?php
+			echo('-->');
+			echo('<h1>PHP is working too!</h1>');
+		?>
+	</body>
+</html>
+	
