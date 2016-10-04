454c2f93135fcac6516fe621e40edd37e9f8c511
Andrew Hutton <fromalaska49@gmail.com>
2016-10-04 13:57:24 -0500

    Updated with Bootstrap

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
-
+<!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="index.css">
+		<link rel="stylesheet" type="text/css" href="index.css" />
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 	</head>
-
 	<body>
 		<?php
 			require('menu.php');
 		?>
-		<main>
-
-			<div class="m-full">
-				<a href="#">
-					<div class="join-acm">Join ACM</div>
-				</a>
+		<center>
+			<div id="content" class="container">
+				<div class="col-sm-12">
+					<div class="m-full">
+						<a href="#">
+							<div class="join-acm">
+								Join ACM
+							</div>
+						</a>
+					</div>
+				</div>
 			</div>
-
-		</main>
-
-
-
-
+		</center>
 	</body>
-
-
 </html>
