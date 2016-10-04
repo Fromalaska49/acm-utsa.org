351e742e3f731e3c7ce6821791a433f4cac5bcf6
Andrew Hutton <fromalaska49@gmail.com>
2016-10-04 11:39:50 -0500

    Moved code to index.php

--- index.php
+++ index.php
@@ -1,24 +1,46 @@
+
 <html>
 	<head>
-		<title>
-			Hello World
-		</title>
+		<link rel="stylesheet" type="text/css" href="index.css">
 	</head>
+
 	<body>
-		<h1>
-			The server is working!
-		</h1>
-		<br />
-		<?php
-			echo('<!--');
-		?>
-		<p>
-			Error: PHP isn't working!
-		</p>
-		<?php
-			echo('-->');
-			echo('<h1>PHP is working too!</h1>');
-		?>
+		<nav>
+			<div class="tag">
+				<a href="." class="logo">
+					<img src="images/logo.png" style="width:160px;margin:20px;" />
+				</a>
+				<h1> Association for Computing Machinery </h1>
+			</div>	
+			<menu>
+				<a href="#">Home</a>
+				<a href="#">About</a>
+				<a href="#">Groups</a>
+				<a href="#">Careers</a>
+				<a href="#">Officers</a>
+				<a href="#">Contact US</a>
+				<a href="http://rowdyhacks.com">RowdyHacks</a>
+
+			</menu>
+
+
+		</nav>
+
+
+		<main>
+
+			<div class="m-full">
+				<a href="#">
+					<div class="join-acm">Join ACM</div>
+				</a>
+			</div>
+
+		</main>
+
+
+
+
 	</body>
+
+
 </html>
-	
