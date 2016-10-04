c6c45bfe12613bb8bdf8e5781351b742d268c965
Andrew Hutton <fromalaska49@gmail.com>
2016-10-04 18:31:19 -0500

    Separated require code from head

---
+++ head-includes.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="index.css" />
+<script type="text/javscript">
+	$(document).ready(function(){
+		$("#sub-menu").css("position", "relative");
+		$("#sub-menu").css("right", $(document).width() - ($('#content').offset().left + $('#content').width()));
+	});
+	$(window).on("resize",function(){
+		$("#sub-menu").css("position", "relative");
+		$("#sub-menu").css("right", $(document).width() - ($('#content').offset().left + $('#content').width()));
+	});
+</script>
--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<link rel="stylesheet" type="text/css" href="index.css" />
+		<?php
+			require('head-includes.php');
+		?>
 	</head>
 	<body>
 		<?php
