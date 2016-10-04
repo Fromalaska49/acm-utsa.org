d1ebf6d90e5ad20b1a2fed6e79d21f2a1b36885c
Andrew Sanetra <andrew@acm-utsa.org>
2016-10-04 09:49:20 -0500

    Converted most of the page to bootstrap

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="index.css" />
 		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="index.css" />
 	</head>
 	<body>
 		<?php
@@ -12,13 +12,13 @@
 		<center>
 			<div id="content" class="container">
 				<div class="col-sm-12">
-					<div class="m-full">
+					<!--<div class="m-full">
 						<a href="#">
 							<div class="join-acm">
 								Join ACM
 							</div>
 						</a>
-					</div>
+					</div>-->
 				</div>
 			</div>
 		</center>
