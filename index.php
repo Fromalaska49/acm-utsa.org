8faa8e6c1fe1dc78e8a4817bdfbf7d072b4aa25b
Andrew Hutton <fromalaska49@gmail.com>
2016-10-04 14:12:22 -0500

    Added Bootstrap JS

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<head>
 		<link rel="stylesheet" type="text/css" href="index.css" />
 		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<?php
