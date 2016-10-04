ae64bc24757606d52416dcce5f6fe8e85f4a8c95
Andrew Hutton <fromalaska49@gmail.com>
2016-10-04 12:21:08 -0500

    Added sub menu

--- menu.php
+++ menu.php
@@ -1,10 +1,11 @@
+	<div id="menu-container">
 		<nav>
 			<div class="tag">
 				<a href="." class="logo">
 					<img src="images/logo.png" style="width:160px;margin:20px;" />
 				</a>
 				<h1> Association for Computing Machinery </h1>
-			</div>	
+			</div>
 			<menu>
 				<a href="#">Home</a>
 				<a href="#">About</a>
@@ -12,9 +13,12 @@
 				<a href="#">Careers</a>
 				<a href="#">Officers</a>
 				<a href="#">Contact US</a>
-				<a href="http://rowdyhacks.com">RowdyHacks</a>
-
 			</menu>
-
-
 		</nav>
+	</div>
+	<menu id="sub-menu" style="display:block;float:right;">
+		<a href="#" class="sub-menu-item" style="background-color:#3498db;">Join ACM</a>
+		<a href="#" class="sub-menu-item" style="background-color:#2980b9;">Pay Dues</a>
+		<a href="#" class="sub-menu-item" style="background-color:#1f8a70;">Calendar</a>
+		<a href="http://rowdyhacks.com" class="sub-menu-item" style="background-color:#bedb39;">RowdyHacks</a>
+	</menu>
