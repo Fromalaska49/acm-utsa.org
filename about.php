bfc3d925ab4306dea03e8eef74a688ad760ee1aa
Rurigok <asanetras@gmail.com>
2016-10-18 10:57:57 -0500

    Start writing about page content

--- about.php
+++ about.php
@@ -13,7 +13,23 @@
 		<div id="content" class="container">
 			<div class="row">
 				<div class="col-sm-12">
-					<!-- content here! -->
+					<h1>About ACM UTSA</h1>
+                    <p>
+                    	The Association for Computing Machinery at UTSA is a student organization dedicated to
+                        computer science at 
+                        was established on ... by ...
+                        
+                    </p>
+                    <h2>What We Offer</h2>
+                    <p>
+                    	As UTSA's premier computer science student organization, ACM offers:
+                    </p>
+                    <ul>
+                    	<li>Career development and opportunities</li>
+                    	<li>Technical hands-on workshops on various technologies</li>
+                    	<li>Social events, hackathons, and competitions</li>
+                    	<li>A fun &amp; welcoming atmosphere for all members</li>
+                    </ul>
 				</div>
 			</div>
 		</div>
