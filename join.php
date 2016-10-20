6f06c4c09c3774e63e3b30434a87a11204e010eb
Rurigok <asanetras@gmail.com>
2016-10-20 10:19:23 -0500

    Add basic wording to join page and tweak index headers

--- join.php
+++ join.php
@@ -14,8 +14,21 @@
 			<div class="row">
 				<div class="col-sm-12">
 					<!-- content here! -->
+					<h1>Joining ACM</h1>
 					<p>
-
+						Membership in ACM UTSA is on a semester-basis.
+						There are only two simple membership requirements:
+					</p>
+					<ul>
+						<li>Attend our general meetings</li>
+						<li><a href="dues">Pay our membership dues</a></li>
+					</ul>
+					<p>
+						Once you pay your dues, you will become a full member of
+						ACM UTSA and enjoy all of the benefits. We strongly believe
+						that membership participation is vital to creating a fun
+						and engaging experience, so the only thing we ask of our
+						members is to actively participate in our meetings and events.
 					</p>
 				</div>
 			</div>
