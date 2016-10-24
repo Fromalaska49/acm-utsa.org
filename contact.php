7232d52a5093bb4578f60c0006df6a87c3ded8c1
Andrew Bennett <abennett0322@gmail.com>
2016-10-24 11:02:00 -0500

    update contact page
    added the button to add to slack channel

--- contact.php
+++ contact.php
@@ -18,7 +18,9 @@
 					<p>
 						ACM's primary form of communication is Slack (acm-utsa.slack.com).
 					</p>
-					[add to slack button]
+					<a href="http://acm-utsa.slack.com">
+						<input type="button" style="background:url('images/icons/add-to-slack.png');width:143px;background-size:cover;border-radius:5px;" />
+					</a>
 					<p>
 						For any general inquiries, comments, questions, or any other business, please
 						contact us at <a href="mailto:acm.utsa@gmail.com">acm.utsa@gmail.com</a>.
